ff0f5261ad5ba2af00a15b584c1dd0fc31d39eed
rithik <115656594+theRithik@users.noreply.github.com>
2023-05-22 22:31:28 +0530

    Add files via upload

---
+++ actions/actionFile.js
@@ -0,0 +1,11 @@
+export function movieList(){
+
+    return{
+        type:'MOVIE_LIST',
+        payload:[
+            {id:1,name:'avengers'},
+            {id:2,name:'jab we mat'},
+            {id:3,name:'mad max'}
+        ]
+    }
+}
